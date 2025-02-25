bd3d319a7708151d42de97f308b282b4cd57b2e9
DARLY GORELLE <mrsdianghadg@gmail.com>
2025-02-25 16:57:02 +0100

    feat: suite formulaire en temps reel

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,28 @@
-// Load jQuery
+// Informations personnelles
 $( "#fullname" ).on( "keyup", function( event ) {
-        $("#templateName").text(event.target.value) ;
+    $("#templateName").text(event.target.value) ;
 } );
 
+$( "#telephone" ).on( "keyup", function( event ) {
+    $("#templatePhone").text(event.target.value) ;
+} );
+$( "#address" ).on( "keyup", function( event ) {
+    $("#templateAddress").text(event.target.value) ;
+} );
+$( "#age" ).on( "keyup", function( event ) {
+    $("#templateAge").text(event.target.value) ;
+} );
+
+)
+$( "#email" ).on( "keyup", function( event ) {
+    $("#templateEmail").text(event.target.value) ;
+} );
+
+$( "#titre" ).on( "keyup", function( event ) {
+    $("#templateTitre").text(event.target.value) ;
+} );
+
+// Experiance professionelle
+$( "#companyName" ).on( "keyup", function( event ) {
+    $("#templateName").text(event.target.value) ;
+} );
