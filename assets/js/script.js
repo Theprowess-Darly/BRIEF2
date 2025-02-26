3850a62dfd74a95600cbf7b1c6677679ea9f2d70
DARLY GORELLE <mrsdianghadg@gmail.com>
2025-02-26 05:59:38 +0100

    feat: fin du formulaire

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,25 +2,36 @@
 $( "#fullname" ).on( "keyup", function( event ) {
     $("#templateName").text(event.target.value) ;
 } );
-
-$( "#telephone" ).on( "keyup", function( event ) {
-    $("#templatePhone").text(event.target.value) ;
+$( "#age" ).on( "keyup", function( event ) {
+    $("#templateAge").text(event.target.value) ;
+} );
+$( "#sex" ).on( "keyup", function( event ) {
+    $("#templateSex").text(event.target.value) ;
 } );
 $( "#address" ).on( "keyup", function( event ) {
     $("#templateAddress").text(event.target.value) ;
 } );
-$( "#age" ).on( "keyup", function( event ) {
-    $("#templateAge").text(event.target.value) ;
+$( "#telephone" ).on( "keyup", function( event ) {
+    $("#templatePhone").text(event.target.value) ;
 } );
-
-)
+$( "#picture" ).on( "keyup", function( event ) {
+    $("#templatePicture").text(event.target.value) ;
+} );
 $( "#email" ).on( "keyup", function( event ) {
     $("#templateEmail").text(event.target.value) ;
 } );
-
 $( "#titre" ).on( "keyup", function( event ) {
     $("#templateTitre").text(event.target.value) ;
 } );
+$( "#situation" ).on( "keyup", function( event ) {
+    $("#templateStatus").text(event.target.value) ;
+} );
+$( "#description" ).on( "keyup", function( event ) {
+    $("#templateDescription").text(event.target.value) ;
+} );
+
+
+
 
 // Experiance professionelle
 $( "#companyName" ).on( "keyup", function( event ) {
